65fd85703113232b5fc006d82b58123ab90209ab
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-07-20 17:22:36 -0300

    Update AluguelController.java

--- src/main/java/com/imobiliaria/controller/AluguelController.java
+++ src/main/java/com/imobiliaria/controller/AluguelController.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/aluguels")
+@Path("/alugueis")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AluguelController {
